4fcda5d883acd0a982a3a04fa3383c0db43261d2
Missy Westland <melissa.westland.16@gmail.com>
2021-02-13 16:54:12 -0500

    many links - procedurally generated

--- generate.py
+++ generate.py
@@ -1,9 +1,19 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
+
 length = 1
-width = 2
-height = 3
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length,width,height])
+width = 1
+height = 1
+x = 0
+y = 0 
+z = 1
+for i in range(0,10):
+	pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+	length = .9 * length
+	width = .9 * width
+	height = .9 * height
+	z+=height
+	
 pyrosim.End()
 
