6cc84b0f9292767f27635569bfdf63acd9bd7ca6
Missy Westland <melissa.westland.16@gmail.com>
2021-02-13 16:29:04 -0500

    many links

---
+++ simulate.py
@@ -0,0 +1,18 @@
+
+import pybullet_data
+import pybullet as p
+import time
+
+physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("box.sdf")
+
+for x in range(0, 1000):
+	time.sleep(0.0166667)
+	p.stepSimulation()
+
+
+p.disconnect()
